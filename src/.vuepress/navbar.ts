f55dd5c23a330cc64211c09e9d9187ea72e7eb56
floweryu <floweryu@qq.com>
2023-12-26 17:00:06 +0800

    update

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -9,52 +9,42 @@ export default navbar([
   },
   {
     text: "中间件",
-    icon: "book",
     children: [
       {
         text: "MySQL",
-        icon: "fa-solid fa-signature",
         link: "/mysql/",
       },
       {
         text: "Redis",
-        icon: "fa-solid fa-signature",
         link: "/redis/",
       },
       {
         text: "Sentinel",
-        icon: "fa-solid fa-signature",
         link: "/sentinel/",
       },
     ],
   },
   {
     text: "Spring",
-    icon: "fa-solid fa-chart-simple",
     link: "/spring/",
   },
   {
     text: "RocketMQ",
-    icon: "fa-solid fa-chart-simple",
     link: "/rocketmq/",
   },
   {
     text: "Dubbo",
-    icon: "fa-solid fa-chart-simple",
     link: "/dubbo/",
   },
   {
     text: "算法笔记",
-    icon: "fa-solid fa-chart-simple",
     link: "/algorithm/",
   },
   {
     text: "随笔",
-    icon: "fa-solid fa-chart-simple",
     children: [
       {
         text: "分布式",
-        icon: "fa-solid fa-signature",
         link: "/distributed/",
       },
     ],
